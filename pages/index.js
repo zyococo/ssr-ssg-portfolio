d7688d954df7af8b255aa1014ed37d12ec5b381c
g1348032 <g1348032@icloud.com>
2023-02-26 05:20:48 +0900

    check to see if its working

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Layout, { siteTitle } from '@/components/Layout'
 import Link from "next/link";
